5da4fd2d7368f5b18b58a7518881ab8eb3b70c02
Karl Rohr <karlrohr@annihilation.local>
2016-06-03 19:51:31 -0400

    Added unit testing for server load, and /api/ route

--- test/__api.js
+++ test/__api.js
@@ -1,8 +1,21 @@
 var expect = require('expect');
-var server = require('../src/server.js');
+
+var request = require('supertest');
 describe('API', function(){
+  var server;
+  beforeEach( function(){
+    server = require('../src/server.js');
+  });
+
+  afterEach( function(){
+    server.close();
+  });
    // it() is bullet points 'actual test'
-  it('Should load', function(){
-    expect('daisy').toBe('daisy');
+  it('Should load', function(done){
+    request(server).get('/api/').set('Accept', 'application/json').expect('Content-type', /json/, done);
+  });
+
+  it('/api/ should return home: true', function(done){
+    request(server).get('/api/').set('Accept', 'application/json').expect('Content-type', /json/).expect(200, {home: 'true'}, done);
   });
 });

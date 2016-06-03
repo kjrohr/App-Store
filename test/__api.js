ebc00dbb7689fec1e56cc44f0f088dd7c7b07587
Karl Rohr <karlrohr@annihilation.local>
2016-06-03 19:37:44 -0400

    added expect, test folder, and __api file for unit testing

---
+++ test/__api.js
@@ -0,0 +1,7 @@
+var expect = require('expect');
+describe('API', function(){
+   // it() is bullet points 'actual test'
+  it('Should load', function(){
+    expect('daisy').toBe('daisy');
+  });
+});

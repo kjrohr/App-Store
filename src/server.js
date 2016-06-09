f6a377c5208d5678f855fc30b82f5870073edaf5
Karl Rohr <karlrohr@annihilation.local>
2016-06-09 18:59:47 -0400

    Added example debugging to server.js file

--- src/server.js
+++ src/server.js
@@ -4,12 +4,16 @@ var express = require('express');
 // Sets body_parser to the module body-parser
 var body_parser = require('body-parser');
 
+// Sets the util varaible to the util.js file
+const util = require('../lib/util.js');
+
 // Sets app to the express
 var app = express();
 
 // Sets port to 3000
 var port = 3000;
 
+// Allows the app to use the body parser module to parse body data like req.body
 app.use(body_parser.json());
 
 // Tells the app to have a requirement of /api and then require the api.js file and pass it express
@@ -18,7 +22,8 @@ app.use('/api', require('./routes/api.js')(express));
 
 // What the server does while it's running and initialized
 var server = app.listen(port, function(){
-  console.log("Server is running on port: " + port);
+  util.debug("Server is running on port: ", port);
 });
 
+// exports the server as variable to be used when this file is included
 module.exports = server;

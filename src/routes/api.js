15765e71d1d3bd15d34e429dac4f356a89d16909
Karl Rohr <karlrohr@annihilation.local>
2016-06-09 17:51:54 -0400

    Got course CRUD working

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,5 @@
 const user = require('../models/user');
+const course = require('../models/course');
 module.exports = (express) => {
   const router = express.Router();
 
@@ -50,6 +51,60 @@ module.exports = (express) => {
   router.post('/users', (req, res) => {
     console.log(req.body);
     user.create(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Read All
+  router.get('/courses', (req, res) => {
+    course.findAll((err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+
+    });
+  });
+
+  // Read One
+  router.get('/courses/:id', (req, res) => {
+    // URL id will override any preexisting id
+    var x = {};
+    x.id = req.params.id;
+    course.find(x, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Delete
+  router.delete('/courses/:id', (req, res) => {
+    // URL id will override any preexisting id
+    req.body.id = req.params.id;
+    course.destroy(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Update
+  router.post('/courses/:id', (req, res) => {
+    // URL id will override any preexisting id
+    req.body.id = req.params.id;
+    course.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Create User
+  router.post('/courses', (req, res) => {
+    console.log(req.body);
+    course.create(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);

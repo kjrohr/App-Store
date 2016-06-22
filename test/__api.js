523c9b84339162c41e4a54596c6475cac4b9681d
Karl Rohr <karlrohr@annihilation.local>
2016-06-22 00:24:36 -0400

    Commented out everything in api testing

--- test/__api.js
+++ test/__api.js
@@ -1,20 +1,20 @@
 // const expect = require('expect');
-const request = require('supertest');
+
 // const routes = [
 //   { title: '/api/users should return all users', route: '/api/users', status: 200 },
 //   { title: '/api/users/3 should return Lindsay', route: '/api/users/3', status: 200 },
 //   { title: '/api/apps should return all apps', route: '/api/apps', status: 200 },
 //   { title: '/api/apps/1', route: '/api/apps/1', status: 200 },
 // ];
-describe('API', () => {
-  let server;
-  beforeEach(() => {
-    server = require('../src/server.js');
-  });
-
-  afterEach(() => {
-    server.close();
-  });
+// describe('API', () => {
+//   let server;
+//   beforeEach(() => {
+//     server = require('../src/server.js');
+//   });
+//
+//   afterEach(() => {
+//     server.close();
+//   });
 
   // Don't have a route for just /api
   //  // it() is bullet points 'actual test'
@@ -31,21 +31,21 @@ describe('API', () => {
   //   .expect(200, { home: 'true' }, done);
   // });
 
-  it('/api/users/id should return a user object with Lindsay', (done) => {
-    const fakeUserId = 3;
-    request(server)
-    .get('/api/users/' + fakeUserId)
-    .set('Accept', 'application/json')
-    .expect('Content-type', /json/)
-    .expect(200, {
-      age: 22,
-      createdAt: '2016-06-09T01:36:20.000Z',
-      hobby: 'witcher',
-      id: 3,
-      name: 'Lindsay',
-      updatedAt: '2016-06-12T04:46:52.000Z',
-    }, done);
-  });
+  // it('/api/users/id should return a user object with Lindsay', (done) => {
+  //   const fakeUserId = 3;
+  //   request(server)
+  //   .get('/api/users/' + fakeUserId)
+  //   .set('Accept', 'application/json')
+  //   .expect('Content-type', /json/)
+  //   .expect(200, {
+  //     age: 22,
+  //     createdAt: '2016-06-09T01:36:20.000Z',
+  //     hobby: 'witcher',
+  //     id: 3,
+  //     name: 'Lindsay',
+  //     updatedAt: '2016-06-12T04:46:52.000Z',
+  //   }, done);
+  // });
 
   // Dynamic Route Api Unit Testing Inbound
   // for (let index = 0; index < routes.length; index++) {
@@ -54,4 +54,4 @@ describe('API', () => {
   //     .get(routes[index].route)
   //   });
   // }
-});
+// });

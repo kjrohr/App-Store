d994d21929f9f76a2213c23381b262972084622a
Karl Rohr <kjrohr@fullsail.edu>
2016-06-24 18:06:30 -0400

    gulp commit

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('hello', () => {
 });
 
 gulp.task('default', ['add', 'commit', 'push'], () => {
-  console.log('default');
+  console.log('Finishing push');
 });
 
 // aslkajsdlkjasdlkjasdasdasdkjhdsfkjh

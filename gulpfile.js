e89c786a6db9c49489c2477122cd1f385bba5b97
Karl Rohr <karlrohr@annihilation.local>
2016-06-24 17:49:13 -0400

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
 const gulp = require('gulp');
 const git = require('gulp-git');
-
-// const packageJSON = require('./package.json');
-
 gulp.task('bump', () => {
-  return gulp.src('./*').pipe(git.add({ args: ' -f' })).pipe(git.commit('gulp commit'))
-  .pipe(git.push('github', 'release'));
+  // Get Yargs from the command line
+  // Get current version from pkg.json
+  // increase version number with utility tool
+  // save new version back to pkg.json
+  // git add
+  // git commit
+  // git tag
+  // git push to release
+
+});
+
+gulp.task('add', () => {
+  console.log('eslint does not like just a return');
+  return gulp.src('./git-test/*')
+    .pipe(git.add());
+});
+
+gulp.task('commit', () => {
+  console.log('eslint does not like just a return');
+  return gulp.src('./*')
+    .pipe(git.commit('initial commit'));
+});
+
+gulp.task('push', () => {
+  git.push('github', 'release', (err) => {
+    if (err) throw err;
+  });
 });
 
 gulp.task('hello', () => {
   console.log('hello');
 });
 
-gulp.task('default', ['bump'], () => {
+gulp.task('default', ['add', 'commit', 'push'], () => {
   console.log('default');
 });
